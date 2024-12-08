4361b8aecadc5d2286d3815646fabc8c12b1dc62
teuz <matheus.castro09@aluno.ifce.edu.br>
2024-12-08 19:23:24 -0300

    Delete teuz directory

--- teuz/perimetro.py
+++
@@ -1,7 +0,0 @@
-def calcular_perimetro(lado: float):
-    calc = lado * 4
-    return calc
-
-lado = float(input('Digite o lado: '))
-resultado = calcular_perimetro(lado)
-print('Seu perimêtro é', resultado)
