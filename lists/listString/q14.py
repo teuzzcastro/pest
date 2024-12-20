7f92293243bf03456eac600a0aaa8739fdb6dca7
teuz <matheus.castro09@aluno.ifce.edu.br>
2024-12-20 11:08:23 -0300

    Delete lists/listString directory

--- lists/listString/q14.py
+++
@@ -1,6 +0,0 @@
-def inverter_direita(string):
-    metade = len(string) // 2
-    return string[:metade] + string[metade:][::-1]
-
-saida = inverter_direita('Pythonista')
-print(saida)
--- lists/listString/q3.py
+++
@@ -1,5 +0,0 @@
-def reverter(string : str):
-    return string[::-1]
-
-saida = reverter('Python')
-print(saida)
