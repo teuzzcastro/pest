d6c2349a5304673ad0bfe2baad68ee54db19d5bd
teuz <matheus.castro09@aluno.ifce.edu.br>
2024-12-20 11:08:51 -0300

    Add files via upload

---
+++ lists/listString/q5.py
@@ -0,0 +1,5 @@
+def primeiros_n(string, n):
+    return string[:n]
+
+saida = primeiros_n('Programar', 5)
+print(saida)
---
+++ lists/listString/q9.py
@@ -0,0 +1,6 @@
+def comprimeto_string(string):
+    resultado = len(string)
+    return resultado
+
+saida = comprimeto_string('Python')
+print(saida)
